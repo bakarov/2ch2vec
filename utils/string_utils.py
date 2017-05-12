c5bf1c8ddd912934106024dcf942502ebed20ea5
bakarov <amirbakarov@gmail.com>
2017-05-12 11:13:41 +0700

    Fix preprocess in the algorithm

--- utils/string_utils.py
+++ utils/string_utils.py
@@ -1,13 +1,17 @@
 from re import sub, compile
 from os import path
 from pymorphy2 import MorphAnalyzer
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import RegexpTokenizer
 
+alpha_tokenizer = RegexpTokenizer('[A-Za-zА-Яа-я]\w+')
 morph = MorphAnalyzer()
-
 
 with open(path.join('corpus', 'stopwords.txt')) as f:
     stopwords = set(f.read().splitlines())
+
+def remove_leading(data):
+    r = compile(r'^(op|gt)')
+    return r.sub(r'', data)
 
 
 def remove_html(text):
@@ -15,17 +19,17 @@ def remove_html(text):
     return html.sub('', text)
 
 def make_tokens(text, vocab):
-    return [token for token in word_tokenize(text) if token in vocab]
+    return [token for token in list(alpha_tokenizer.tokenize(text)) if token in vocab]
 
 
 def morph_parse(text):
-    return ' '.join([morph.parse(word)[0].normal_form for word in word_tokenize(text) if morph.parse(word)[0].normal_form not in stopwords])
-
+    return ' '.join([morph.parse(word)[0].normal_form for word in list(alpha_tokenizer.tokenize(text)) if morph.parse(word)[0].normal_form not in stopwords])
 
 def cut(text):
-    r = compile(r'\(OP\)|&#(\d*);|&quot;|&gt;|&#47;|(http|https):.*')
-    return r.sub('', make_alpha(remove_html(punctuate_sent(punctuate_word(text))))).lower().strip()
-
+    r = compile(r'\(OP\)|gt|quot|&#(\d*);|&quot;|&gt;|&#47;|(http|https):.*')
+    return remove_leading(' '.join(r.sub('',
+    make_alpha(remove_html(punctuate_sent(punctuate_word(text))))).
+                                   lower().strip().split()))
 
 def make_alpha(text):
     alpha = compile('[^a-zа-яA-ZА-Я,\.\?! ]')
