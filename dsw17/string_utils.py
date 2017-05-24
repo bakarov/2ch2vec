ac05fba5ebc82aa7376b3acc6bbbe61ac15ab43f
bakarov <amirbakarov@gmail.com>
2017-05-24 23:17:28 +0700

    Fix algorithm

--- dsw17/string_utils.py
+++ dsw17/string_utils.py
@@ -9,6 +9,7 @@ morph = MorphAnalyzer()
 with open('stopwords.txt') as f:
     stopwords = set(f.read().splitlines())
 
+
 def remove_leading(data):
     r = compile(r'^(op|gt)')
     return r.sub(r'', data)
@@ -18,6 +19,7 @@ def remove_html(text):
     html = compile(r'<.*?>')
     return html.sub('', text)
 
+
 def make_tokens(text, vocab):
     return [token for token in list(alpha_tokenizer.tokenize(text)) if token in vocab]
 
@@ -25,11 +27,13 @@ def make_tokens(text, vocab):
 def morph_parse(text):
     return ' '.join([morph.parse(word)[0].normal_form for word in list(alpha_tokenizer.tokenize(text)) if morph.parse(word)[0].normal_form not in stopwords])
 
+
 def cut(text):
     r = compile(r'\(OP\)|gt|quot|&#(\d*);|&quot;|&gt;|&#47;|(http|https):.*')
     return remove_leading(' '.join(r.sub('',
     make_alpha(remove_html(punctuate_sent(punctuate_word(text))))).
                                    lower().strip().split()))
+
 
 def make_alpha(text):
     alpha = compile('[^a-zа-яA-ZА-Я,\.\?! ]')
